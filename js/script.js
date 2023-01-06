ee380af91d42c82af8aecaf7163fe42ed92c2c55
Andrei Patskou <apatskou@gmail.com>
2023-01-06 19:16:48 +0300

    the first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// let category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Andrei';
+
+// alert(`Hello, ${user}`);
+
+console.log(4 + ' - object');
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+
+incr++;
+decr--;
+
+console.log(incr + ' ' + decr);
+
+console.log(5%2);
